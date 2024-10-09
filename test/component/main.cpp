4b4d5fc9a7f2bb395acbc519e41362832adefb69
vicente <munozvicente@MBP-i9.local>
2024-10-09 10:13:39 -0300

    initial commit

---
+++ test/component/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Securics urlRequest test component
+ * Copyright (C) 2023-2024, RV Bionics Group SpA.
+ * July 18, 2022.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/unit/mocks/MockRequest.hpp
@@ -0,0 +1,34 @@
+/*
+ * Securics urlRequest test component
+ * Copyright (C) 2023-2024, RV Bionics Group SpA.
+ * July 11, 2022.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _MOCKREQUEST_HPP
+#define _MOCKREQUEST_HPP
+
+#include "urlRequest.hpp"
+
+/**
+ * @brief This class is a wrapper for the cURL library.
+ */
+template<typename T>
+class MockRequest : public cURLRequest<MockRequest<T>, T>
+{
+public:
+    /**
+     * @brief Constructor.
+     * @param requestImplementator The request implementator instance.
+     */
+    explicit MockRequest(std::shared_ptr<IRequestImplementator> requestImplementator)
+        : cURLRequest<MockRequest<T>, T>(requestImplementator)
+    {
+    }
+};
+
+#endif // _MOCKREQUEST_HPP
